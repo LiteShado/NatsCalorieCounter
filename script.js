cc0945203259ea3e27617c07013b733b7d0ae4c0
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2024-09-29 01:43:55 -0400

    input element added to template literal

--- script.js
+++ script.js
@@ -30,5 +30,6 @@ function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
     console.log(entryNumber);
-    const HTMLString = `<label>Entry ${entryNumber} Name</label>`;
+    const HTMLString = ` <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">`;
   }
