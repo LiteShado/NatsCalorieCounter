cd365f6fc4c82a05949869e4485707ff0dfc2d35
Nat Richmond <natrichmond@Nats-MBP.lan>
2024-10-01 21:02:38 -0400

    function updated

--- script.js
+++ script.js
@@ -79,6 +79,31 @@ function addEntry() {
       const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
       const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
       const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+      const budgetCalories = getCaloriesFromInputs([budgetNumberInput]); //this parameter is passed within an array because the NodeList is necessary
+
+      if (isError) {
+        return //this will end the function if an invalid input is detected
+      }
+      const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+      const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+      const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit"; // ternary operator will decide what surplusOrDeficit will be
+
+      output.innerHTML = `<span class="${surplusOrDeficit.value.toLowerCase()};
+
+      output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+      
+      <hr></hr>
+      <p>${budgetCalories} Calories Budgeted</p>
+      <p>${consumedCalories} Calories Consumed</p>
+      <p>${exerciseCalories} Calories Burned</p>
+      
+      output.classList.remove('hide');
+      `;
+
 
 
     }
+  } 
+
+}
+
